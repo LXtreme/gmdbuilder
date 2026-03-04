45b0dc7e9bb3c1b987345ca685ec43bda5d40751
Xtreme <luqmancraft145@gmail.com>
2026-03-04 15:49:16 -0500

    updated typemap

--- src/gmdbuilder/object_types.py
+++ src/gmdbuilder/object_types.py
@@ -4,6 +4,8 @@ from typing import Literal, TypedDict, Required, Any
 from gmdkit.models.prop.hsv import HSV
 from gmdkit.models.prop.particle import Particle
 
+BoolLike = bool | Literal[0,1]
+
 EASING = Literal[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 XY_ONLY = Literal[0,1,2]
 COLOR1P9 = Literal[0,1,2,3,4,5,6,7,8]
